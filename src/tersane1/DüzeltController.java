dbc12cf2a833fd951fdb2677cf260253dc29cf5f
HP <HP@LAPTOP-2ODONNHD>
2020-04-27 17:36:23 +0300

    Çalışan ekle,güncelle ve sil classlarında geliştirmeler yapıldı hatalar düzeltildi.
    30dk çalışıldı.

--- src/tersane1/DüzeltController.java
+++ src/tersane1/DüzeltController.java
@@ -5,6 +5,7 @@
  */
 package tersane1;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -13,10 +14,16 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import tersane1.data.database;
 
 /**
@@ -36,13 +43,31 @@ public class DüzeltController implements Initializable {
     private TextField level;
     @FXML
     private Button güncelle;
+    @FXML
+    private Button anasayfa;
+    @FXML
+    private Label oldumu;
 
     /**
      * Initializes the controller class.
      */
     @FXML
     public void HandleButtonAction(ActionEvent event){
-        
+        if(event.getSource()==anasayfa){
+            try{
+                    Node node=(Node) event.getSource();
+                    Stage stage=(Stage) node.getScene().getWindow();
+                    stage.close();
+
+                    Scene scene=new Scene(FXMLLoader.load(getClass().getResource("FXMLDocument.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+
+
+                }catch(IOException e){
+                    System.err.println(e.getMessage());
+                }
+        }
     }
     
     
@@ -59,21 +84,23 @@ public class DüzeltController implements Initializable {
         String soyad=soy.getText();
         String lev=level.getText();
         
-        String sql="UPDATE CALISANLAR SET AD=?,SOYAD=?,SEVIYE=?  WHERE ID=?";
         
         
         try {
-            ps=database.connection.prepareStatement(sql);
+            String a=database.update();
+            ps=database.connection.prepareStatement(a);
             
             ps.setString(1, ad);
             ps.setString(2, soyad);
             ps.setString(3, lev);
             ps.setString(4, ıd);
             ps.executeUpdate();
-                
+            oldumu.setTextFill(Color.GREEN);
+            oldumu.setText("BAŞARIYLA GÜNCELLENDİ");    
         } catch (SQLException ex) {
             Logger.getLogger(SilController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     
 }
